110973a4d9ad82be3db1e160c53966f3cc3257a3
Chae <chae03yb@gmail.com>
2024-07-16 14:56:44 +0900

    merge branch

---
+++ src/components/allergyHighlighter.vue
@@ -0,0 +1,26 @@
+<script>
+export default {
+  data() {
+    return {
+      allergy: null,
+    };
+  },
+  methods: {
+    toggleHighlight() {
+
+    }
+  }
+}
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+.allergyTrigger {
+  color: red;
+  font-weight: bold;
+}
+
+</style>
