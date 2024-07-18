30696e3933e117bcdbc7436def9181518f005378
신우찬 <8621skyblue@naver.com>
2024-07-18 23:03:04 +0900

    fixed untracked files

--- src/App.vue
+++ src/App.vue
@@ -1,91 +1,103 @@
-<template> <!-- HTML -->
+<template>
+  <!-- HTML -->
   <div id="app">
-    <h1>오성고 시간표</h1>
-
-    <table style="width: 45%; height: 85px; margin:auto"> <!-- 너비 -->
-      <caption style="text-align: center;"><input type="text" size="3">의 2024년도 1학기의 시간표 입니다.</caption>
-      <tr>
-        <th></th>
-        <th>월</th>
-        <th>화</th>
-        <th>수</th>
-        <th>목</th>
-        <th>금</th>
-      </tr>
-      <!-- 테이블의 내용 -->
-      <tr>
-        <th>1교시</th>
-        <td> <input type="text" size="3"> </td>
-        <td> <input type="text" size="3"> </td>
-        <td> <input type="text" size="3"> </td>
-        <td> <input type="text" size="3"> </td>
-        <td> <input type="text" size="3"> </td>
-      </tr>
-      <tr>
-        <th>2교시</th>
-        <td> <input type="text" size="3"> </td>
-        <td> <input type="text" size="3"> </td>
-        <td> <input type="text" size="3"> </td>
-        <td> <input type="text" size="3"> </td>
-        <td> <input type="text" size="3"> </td>
-      </tr>
-      <tr>
-        <th>3교시</th>
-        <td> <input type="text" size="3"> </td>
-        <td> <input type="text" size="3"> </td>
-        <td> <input type="text" size="3"> </td>
-        <td> <input type="text" size="3"> </td>
-        <td> <input type="text" size="3"> </td>
-      </tr>
-      <tr>
-        <th>4교시</th>
-        <td> <input type="text" size="3"> </td>
-        <td> <input type="text" size="3"> </td>
-        <td> <input type="text" size="3"> </td>
-        <td> <input type="text" size="3"> </td>
-        <td> <input type="text" size="3"> </td>
-      </tr>
-      <tr>
-        <th>5교시</th>
-        <td> <input type="text" size="3"> </td>
-        <td> <input type="text" size="3"> </td>
-        <td> <input type="text" size="3"> </td>
-        <td> <input type="text" size="3"> </td>
-        <td> <input type="text" size="3"> </td>
-      </tr>
-      <tr>
-        <th>6교시</th>
-        <td> <input type="text" size="3"> </td>
-        <td> <input type="text" size="3"> </td>
-        <td> <input type="text" size="3"> </td>
-        <td> <input type="text" size="3"> </td>
-        <td> <input type="text" size="3"> </td>
-      </tr>
-      <tr>
-        <th>7교시</th>
-        <td> <input type="text" size="3"> </td>
-        <td> <input type="text" size="3"> </td>
-        <td> <input type="text" size="3"> </td>
-        <td> <input type="text" size="3"> </td>
-        <td> <input type="text" size="3"> </td>
-      </tr>
-      <tr>
-        <th>8교시</th>
-        <td> <input type="text" size="3"> </td>
-        <td> <input type="text" size="3"> </td>
-        <td> <input type="text" size="3"> </td>
-        <td> <input type="text" size="3"> </td>
-        <td> <input type="text" size="3"> </td>
-      </tr>
-    </table>
+    <h1>오성고 시간표/급식표</h1>
+    <div id="main">
+      <div id="time_table">
+        <table style="width: 45%; height: 85px; margin: auto">
+          <!-- 너비 -->
+          <caption style="text-align: center">
+            <input type="text" size="3" />의 2024년도 1학기의 시간표 입니다.
+          </caption>
+          <tr>
+            <th></th>
+            <th>월</th>
+            <th>화</th>
+            <th>수</th>
+            <th>목</th>
+            <th>금</th>
+          </tr>
+          <!-- 테이블의 내용 -->
+          <tr>
+            <th>1교시</th>
+            <td><input type="text" size="3" /></td>
+            <td><input type="text" size="3" /></td>
+            <td><input type="text" size="3" /></td>
+            <td><input type="text" size="3" /></td>
+            <td><input type="text" size="3" /></td>
+          </tr>
+          <tr>
+            <th>2교시</th>
+            <td><input type="text" size="3" /></td>
+            <td><input type="text" size="3" /></td>
+            <td><input type="text" size="3" /></td>
+            <td><input type="text" size="3" /></td>
+            <td><input type="text" size="3" /></td>
+          </tr>
+          <tr>
+            <th>3교시</th>
+            <td><input type="text" size="3" /></td>
+            <td><input type="text" size="3" /></td>
+            <td><input type="text" size="3" /></td>
+            <td><input type="text" size="3" /></td>
+            <td><input type="text" size="3" /></td>
+          </tr>
+          <tr>
+            <th>4교시</th>
+            <td><input type="text" size="3" /></td>
+            <td><input type="text" size="3" /></td>
+            <td><input type="text" size="3" /></td>
+            <td><input type="text" size="3" /></td>
+            <td><input type="text" size="3" /></td>
+          </tr>
+          <tr>
+            <th>5교시</th>
+            <td><input type="text" size="3" /></td>
+            <td><input type="text" size="3" /></td>
+            <td><input type="text" size="3" /></td>
+            <td><input type="text" size="3" /></td>
+            <td><input type="text" size="3" /></td>
+          </tr>
+          <tr>
+            <th>6교시</th>
+            <td><input type="text" size="3" /></td>
+            <td><input type="text" size="3" /></td>
+            <td><input type="text" size="3" /></td>
+            <td><input type="text" size="3" /></td>
+            <td><input type="text" size="3" /></td>
+          </tr>
+          <tr>
+            <th>7교시</th>
+            <td><input type="text" size="3" /></td>
+            <td><input type="text" size="3" /></td>
+            <td><input type="text" size="3" /></td>
+            <td><input type="text" size="3" /></td>
+            <td><input type="text" size="3" /></td>
+          </tr>
+          <tr>
+            <th>8교시</th>
+            <td><input type="text" size="3" /></td>
+            <td><input type="text" size="3" /></td>
+            <td><input type="text" size="3" /></td>
+            <td><input type="text" size="3" /></td>
+            <td><input type="text" size="3" /></td>
+          </tr>
+        </table>
+      </div>
+      <div>급식표</div>
+    </div>
   </div>
 </template>
 
-<script> //script
+<script>
+//script
 
 export default {
-  name: 'App'
-}
+  name: "App",
+};
+
+//neis 급식 정보 가져오기
+const neis = require("neis");
 </script>
 
 <style scoped>
@@ -96,7 +108,6 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-
 }
 
 input[type="text"] {
@@ -106,7 +117,9 @@ input[type="text"] {
   border-bottom: 1px solid black;
 }
 
-table, th, td {
+table,
+th,
+td {
   border: 1px solid black;
   border-collapse: collapse;
   text-align: center;
@@ -114,7 +127,8 @@ table, th, td {
   height: 40px;
 }
 
-th, td {
+th,
+td {
   padding: 20px;
 }
 
@@ -122,5 +136,7 @@ th {
   text-align: center;
 }
 
-
+#main {
+  display: flex;
+}
 </style>
